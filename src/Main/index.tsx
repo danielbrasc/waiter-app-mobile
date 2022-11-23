d1124967db5f54e0d70ca276e026d4781f53d150
Daniel <daniel.bras.117@ufrn.edu.br>
2022-11-23 18:54:09 -0300

    feat: added modal component on main component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -10,8 +10,17 @@ import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
+import { useState } from 'react';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -28,11 +37,19 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert()}>
-            Novo Pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
