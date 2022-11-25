1996e7048e1e20bb9c7603eb1e5b69bdc1d3e208
Daniel <daniel.bras.117@ufrn.edu.br>
2022-11-24 23:45:40 -0300

    refactor: changed the logic of filtering products by category

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -7,22 +7,23 @@ import { Text } from '../Text';
 import { Category, Icon } from './styles';
 
 export function Categories() {
-  const [selectedCategories, setSelectedCategories] = useState<Array<string> | []>([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
-  function handleSelectCategories(categoryId: string) {
-    const addCategory = () => setSelectedCategories(selectedCategories => [...selectedCategories, categoryId]);
-    const removeCategory = () => setSelectedCategories(selectedCategories.filter(item => item !== categoryId));
-
+  function handleSelectCategory(categoryId: string) {
     // Selecionar várias categorias
-    !isSelectedCategory(categoryId) ? addCategory() : removeCategory();
+    // const addCategory = () => setSelectedCategories(selectedCategories => [...selectedCategories, categoryId]);
+    // const removeCategory = () => setSelectedCategories(selectedCategories.filter(item => item !== categoryId));
+    // !isSelectedCategory(categoryId) ? addCategory() : removeCategory();
 
-    // Forma de selecionar apenas uma categorias
-    // const category = selectedCategory === categoryId ? '' : categoryId;
+    // Selecionar apenas uma categorias
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
   }
 
-  function isSelectedCategory(categoryId: string): boolean{
-    return !!selectedCategories.find((item) => item === categoryId);
-  }
+  // Verifica se a categoria está selecionada
+  // fun gory(categoryId: string): boolean{
+  //   return !!selectedCategories.find((item) => item === categoryId);
+  // }
 
   return (
     <>
@@ -34,18 +35,18 @@ export function Categories() {
         keyExtractor={category => category._id}
         renderItem={({item: category}) => {
           // Condição para dar foco a categoria selecionada
-          // const isSelected = selectedCategory === category._id;
+          const isSelected = selectedCategory === category._id;
 
           // Condição para dar foco as categorias selecionadas
-          const isSelected = isSelectedCategory(category._id);
+          // const isSelected = isSelectedCategory(category._id);
 
           return (
-            <Category key={category._id} onPress={() => handleSelectCategories(category._id)} >
+            <Category key={category._id} onPress={() => handleSelectCategory(category._id)} >
               <Icon>
                 <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
               </Icon>
 
-              <Text size={14} weight='600' opacity={isSelected ? 1 : 0.5} color={isSelected ? '#D73035' : ''}>
+              <Text size={14} weight='600' opacity={isSelected ? 1 : 0.5}>
                 {category.name}
               </Text>
             </Category>
